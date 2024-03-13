3a7836bcc918dfd712f607f1cb48f85b7bce2c76
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2024-03-13 11:48:27 +0530

    checkpoint profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,6 +10,12 @@ interface PageProps {
 
 }
 
+const DummyData = [
+    { date: "1/3/2024", status: "received" },
+    { date: "2/3/2024", status: "sent" },
+    { date: "3/3/2024", status: "sent" },
+];
+
 const Page: FC<PageProps> = ({ }) => {
     const [cardHolderData, setCardHolderData] = useState<CardHoldersType | undefined>();
     const [isLoading, setIsLoading] = useState(false);
@@ -51,48 +57,65 @@ const Page: FC<PageProps> = ({ }) => {
     if (!cardHolderData) return <p className="pt-36">No data found</p>;
 
     if (status === "authenticated") {
-        return <div className='w-screen h-screen bg-slate-400'>
-            <p>Signed in as {session.user?.role}</p>
+        return (
+            <div className='w-screen h-screen bg-slate-400 flex flex-row justify-between'>
+                <p>Signed in as {session.user?.role}</p>
 
-            {role === "USER" && (
-                <div>
-                    <Card className="p-4">
-                        <CardHeader className="text-black">Card Holder</CardHeader>
-                        <CardDescription className="text-black">Card Holder Details</CardDescription>
-                        <br />
-                        <CardContent>
-                            <ul className="list-none pl-4">
-                                <li>
-                                    <span className="font-medium">ID:</span> {cardHolderData.id}
-                                </li>
-                                <li>
-                                    <span className="font-medium">Name:</span> {cardHolderData.firstName} {cardHolderData.lastName}
-                                </li>
-                                <li>
-                                    <span className="font-medium">Contact:</span>
-                                    <ul className="list-disc pl-2">
-                                        <li>Phone: {cardHolderData.number}</li>
-                                        <li>Email: {cardHolderData.email}</li>
+                {role === "USER" && (
+                    <>
+                        <div className=''>
+                            <Card className="p-4">
+                                <CardHeader className="text-black">Card Holder</CardHeader>
+                                <CardDescription className="text-black">Card Holder Details</CardDescription>
+                                <br />
+                                <CardContent>
+                                    <ul className="list-none pl-4">
+                                        <li>
+                                            <span className="font-medium">ID:</span> {cardHolderData.id}
+                                        </li>
+                                        <li>
+                                            <span className="font-medium">Name:</span> {cardHolderData.firstName} {cardHolderData.lastName}
+                                        </li>
+                                        <li>
+                                            <span className="font-medium">Contact:</span>
+                                            <ul className="list-disc pl-2">
+                                                <li>Phone: {cardHolderData.number}</li>
+                                                <li>Email: {cardHolderData.email}</li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <span className="font-medium">Age:</span> {cardHolderData.age}
+                                        </li>
+                                        <li>
+                                            <span className="font-medium">Gender:</span> {cardHolderData.gender}
+                                        </li>
+                                        <li>
+                                            <span className="font-medium">Address:</span> {cardHolderData.address}
+                                        </li>
+                                        <li>
+                                            <span className="font-medium">Family Count:</span> {cardHolderData.familyCount}
+                                        </li>
                                     </ul>
-                                </li>
-                                <li>
-                                    <span className="font-medium">Age:</span> {cardHolderData.age}
-                                </li>
-                                <li>
-                                    <span className="font-medium">Gender:</span> {cardHolderData.gender}
-                                </li>
-                                <li>
-                                    <span className="font-medium">Address:</span> {cardHolderData.address}
-                                </li>
-                                <li>
-                                    <span className="font-medium">Family Count:</span> {cardHolderData.familyCount}
-                                </li>
-                            </ul>
-                        </CardContent>
-                    </Card>
-                </div>
-            )}
-        </div >
+                                </CardContent>
+                            </Card>
+                        </div>
+
+                        <div>
+                            <h1 className='w-full text-center'>History</h1>
+
+                            <div className='overflow-auto'>
+                                {DummyData.map((data, index) => (
+                                    <div key={index} className="container flex flex-row justify-between bg-gray-500 rounded-md w-96 p-4 mb-4">
+                                        <div>{data.date}</div>
+                                        <div>{data.status}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div >
+        )
     }
 
     return <a href="/api/auth/signin">Sign in</a>
